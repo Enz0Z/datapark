dd99948ced05f12adffb6203e73f3d52c3d565ad
Orteum <miguelfuertesanchez@gmail.com>
2023-05-10 18:55:41 +0200

    Intento de menu
    No me fufa, probare otra cosa en el futuro

--- app/src/main/java/me/datapark/MainActivity.java
+++ app/src/main/java/me/datapark/MainActivity.java
@@ -1,14 +1,48 @@
 package me.datapark;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ActionBarDrawerToggle drawerToggle;
+
+    @SuppressLint({"MissingInflatedId", "ResourceType"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try{
+            drawerLayout = findViewById(R.id.drawer_layout);
+            navigationView = findViewById(R.id.nav_view);
+            drawerToggle = new ActionBarDrawerToggle(this, drawerLayout,R.string.open,R.string.close);
+            drawerLayout.addDrawerListener(drawerToggle);
+            drawerToggle.syncState();
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    return false;
+                }
+            });
+        }catch(Exception e){
+
+        }
+
+
     }
+
+
 }
